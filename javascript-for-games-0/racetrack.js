4bec54cc65035c6c2240def24008c4e6818b4835
Finlay Davidson <finlay.davidson@coderclass.nl>
2020-11-30 14:59:31 +0100

    Remove unnecessary code, and improve readability (slightly)

--- javascript-for-games-0/racetrack.js
+++ javascript-for-games-0/racetrack.js
@@ -2,9 +2,11 @@ class RaceTrack {
 	constructor(innerPos, outerPos) {
 		this.innerWalls = [];
 		this.outerWalls = [];
+
 		for (let i = 0; i < innerPos.length; i++) {
 			this.innerWalls.push(new Wall(innerPos[i]));
 		}
+
 		for (let i = 0; i < outerPos.length; i++) {
 			this.outerWalls.push(new Wall(outerPos[i]));
 		}
@@ -14,6 +16,7 @@ class RaceTrack {
 		for (let i = 0; i < this.innerWalls.length; i++) {
 			this.innerWalls[i].draw();
 		}
+		
 		for (let i = 0; i < this.outerWalls.length; i++) {
 			this.outerWalls[i].draw();
 		}
--- javascript-for-games-0/sketch.js
+++ javascript-for-games-0/sketch.js
@@ -4,7 +4,6 @@ let innerPos;
 let outerPos;
 
 
-// const car1 = new Car();
 const cars = [];
 let raceTrack;
 
@@ -12,6 +11,7 @@ function setup() {
 	createCanvas(1280, 720);
 	background(127);
 	strokeWeight(4);
+
 	innerPos = [
 		[createVector(300, 200), createVector(560, 140)],
 		[createVector(560, 140), createVector(720, 230)],
@@ -48,45 +48,19 @@ function setup() {
 }
 
 function draw() {
-	// drawTrack();
 	push();
 	noStroke();
 	fill(127);
 	rect(0, 0, width, height);
 	pop();
 	raceTrack.draw();
-	// if (cars[0].rotation != cars[0].direction) {
-	// 	cars[0].direction = (cars[0].rotation + cars[0].direction * 31) / 32;
-	// }
-	// if (keyIsDown(DOWN_ARROW) === false && keyIsDown(UP_ARROW) === false) {
-	// 	cars[0].direction = cars[0].rotation;
-	// }
-	// if (keyIsDown(DOWN_ARROW) || keyIsDown(UP_ARROW)) {
-	// 	if (cars[0].rotation != cars[0].direction) {
-	// 		cars[0].direction = (cars[0].rotation + cars[0].direction * 11) / 12;
-	// 	}
-	// }
-	// if (cars[0].direction - cars[0].rotation > -QUARTER_PI / 16 && cars[0].direction - cars[0].rotation < QUARTER_PI / 16) {
-	// 	cars[0].direction = cars[0].rotation;
-	// }
 
 	for (let i = lag - 1; i > 0; i--) {
 		cars[0].direction[i] = cars[0].direction[i - 1];
 	}
 
-	// v1 = createVector(300, 239);
 
-
-	// if (keyIsDown(DOWN_ARROW)) {
-	// 	// if (cars[0].rotation != cars[0].direction) {
-	// 	// 	cars[0].direction = (cars[0].rotation + cars[0].direction * 23) / 24;
-	// 	// }
-	//
 	if (keyIsDown(UP_ARROW)) {
-		// if (cars[0].rotation != cars[0].direction) {
-		// 	cars[0].direction = (cars[0].rotation + cars[0].direction * 23) / 24;
-		// }
-
 		cars[0].invSpeed = cars[0].invSpeed * cars[0].mult;
 
 		cars[0].speed = cars[0].maxSpeed - cars[0].invSpeed;
@@ -107,32 +81,19 @@ function draw() {
 	if (keyIsDown(LEFT_ARROW)) {
 		cars[0].rotation -= QUARTER_PI / 12;
 		cars[0].direction[0] -= QUARTER_PI / 12;
-		// cars[0].direction -= QUARTER_PI / 12;
 	}
 	if (keyIsDown(RIGHT_ARROW)) {
 		cars[0].rotation += QUARTER_PI / 12;
 		cars[0].direction[0] += QUARTER_PI / 12;
-		// cars[0].direction += QUARTER_PI / 12;
 	}
 	for (let i = 0; i < cars.length; i++) {
 		cars[i].draw();
 		if (cars[i].isColliding(raceTrack) === true) {
 			cars[i] = new Car(createVector(400, 150), 0);
-			// console.log("ded");
 		}
 	}
 }
 
-function drawTrack() {
-	noStroke();
-	fill(127);
-	rect(0, 0, width, height);
-	stroke(0);
-	fill(255);
-	rect(125, 200, 950, 400, 75);
-	fill(127);
-	rect(200, 275, 800, 250, 50);
-}
 
 function linesCross(line1, line2) {
 	let x1 = line1[0].x;
@@ -145,6 +106,7 @@ function linesCross(line1, line2) {
 	let x4 = line2[1].x;
 	let y4 = line2[1].y;
 
+
 	let den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 	let t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
 	let u = -(((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den);
@@ -155,8 +117,3 @@ function linesCross(line1, line2) {
 		return false;
 	}
 }
-
-// function drawCar() {
-// 	fill(127, 63);
-// 	rect(0, 0, 50, 25);
-// }
