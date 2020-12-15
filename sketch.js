b2e8c923398db746af126efd8e4ab098f4167f5f
Finlay Davidson <finlay.davidson@coderclass.nl>
2020-12-15 17:51:31 +0100

    Add some more features, and modularise the code slightly
    There is now the possibility to change the speed the game runs at.
    Currently this is hard-coded, but I plan to add a slider for this.
    I also plan to add some more buttons and things to the GUI.
    
    The mutateItem function might change, so that the ranges it
    returns are within -1 and 1, but also so that the mutation is
    varied enough that the neural networks actually get better.

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@
 
 // The amount of cars, the amount of frames the direction lags behind the
 // rotation (used for drifting), and the positions of the walls of the track
-let amount = 255;
+let amount = 256;
 let lag = 8;
 let innerPos;
 let outerPos;
@@ -19,11 +19,185 @@ let generation = 0;
 const cars = [];
 let raceTrack;
 
+let speed = 16;
+let visual = true;
+
 function setup() {
 	createCanvas(1280, 720);
-	background(127);
+	background(191);
 	strokeWeight(4);
 
+	defineTrack();
+
+	raceTrack = new RaceTrack(innerPos, outerPos, checkpoints);
+
+	newGeneration();
+
+	// noLoop();
+}
+
+function draw() {
+	if (keyIsDown(DOWN_ARROW)) {
+		visual = false;
+		// speed = 1;
+	} else if (keyIsDown(UP_ARROW)) {
+		visual = true;
+		// speed = 10;
+	}
+
+	clear();
+	background(191);
+
+	for (let j = 0; j < speed; j++) {
+		calcPos();
+	}
+
+	if (visual) {
+		raceTrack.draw();
+		drawCars();
+	}
+
+	if (deadCars === cars.length) {
+		newGeneration();
+	}
+
+	push();
+	textSize(16);
+	textFont("monospace");
+	text("FPS: " + round(frameRate()), 16, 32);
+	text("cars: " + (amount - deadCars), 16, 64);
+	text("generation: " + generation, 16, 96);
+	pop();
+}
+
+
+function linesCross(line1, line2) {
+	let x1 = line1[0].x;
+	let y1 = line1[0].y;
+	let x2 = line1[1].x;
+	let y2 = line1[1].y;
+
+	let x3 = line2[0].x;
+	let y3 = line2[0].y;
+	let x4 = line2[1].x;
+	let y4 = line2[1].y;
+
+	// Calculate t and u
+	let den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	let tNum = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
+	let uNum = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3));
+	let t = tNum / den;
+	let u = uNum / den;
+
+	// Return true if the intersection is within the two line segments
+	if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function newGeneration() {
+	// Create the correct amount of cars
+	if (cars.length === 0) {
+		for (let i = 0; i < amount; i++) {
+			cars.push(new Car(createVector(360, 120), 0.52, color(255, 31)));
+		}
+	} else {
+		let bestScore = [cars[0].score, 0];
+		let currentscore;
+		for (let i = 0; i < cars.length; i++) {
+			currentScore = cars[i].score;
+			if (currentScore > bestScore[0]) {
+				bestScore = [currentScore, i];
+			}
+		}
+		let nn = cars[bestScore[1]].nn.serialize();
+		cars[amount - 1] = new Car(createVector(360, 120), 0.52, color(0, 0, 255, 127));
+		cars[amount - 1].nn = NeuralNetwork.deserialize(nn);
+		// console.log(nn.serialize());
+
+		for (let i = 0; i < cars.length - 1; i++) {
+			cars[i] = new Car(createVector(360, 120), 0.52, color(255, 31));
+			cars[i].nn = NeuralNetwork.deserialize(nn);
+			// console.log(cars[i].nn.serialize());
+			cars[i].nn.mutate(mutateItem);
+		}
+		// noLoop();
+	}
+
+	generation++;
+	deadCars = 0;
+}
+
+function mutateItem(x) {
+	if (random() < 0.2) {
+		let offset = randomGaussian(0, 512);
+		let newx = x + offset;
+		// if (newx < -1) {
+		// 	newx = -1;
+		// } else if (newx > 1) {
+		// 	newx = 1;
+		// }
+		return newx;
+		// return random(-1, 1);
+	} else {
+		// let newx = randomGaussian(0, 0.1)
+		// return newx;
+		return x;
+	}
+}
+
+// function mutateLittle(x) {
+// 	if (random() < 0.04) {
+// 		let offset = randomGaussian(0, 0.1);
+// 		let newx = x + offset;
+// 		if (newx < -1) {
+// 			newx = -1;
+// 		} else if (newx > 1) {
+// 			newx = 1;
+// 		}
+// 		console.log(newx);
+// 		return newx;
+// 		// return random(-1, 1);
+// 	} else {
+// 		return x;
+// 	}
+// }
+
+function calcPos() {
+	// Cycle through all of the cars
+	for (let i = 0; i < cars.length; i++) {
+		if (cars[i].alive) {
+			// Read the inputs
+			let rays = cars[i].raycast(raceTrack, false);
+			let action = cars[i].think(rays[1]);
+			cars[i].move(action);
+			cars[i].score++;
+
+			// Kill the car if it is hitting the track
+			if (cars[i].isColliding(raceTrack)) {
+				cars[i].alive = false;
+				deadCars++;
+			}
+
+		}
+	}
+}
+
+function drawCars() {
+	for (let i = 0; i < cars.length; i++) {
+		if (cars[i].alive) {
+			// Draw the car and the raycasts
+			cars[i].draw();
+			if (drawRaycasts) {
+				cars[i].raycast(raceTrack, true);
+			}
+		}
+	}
+}
+
+function defineTrack() {
 	// All of the lines of the track
 
 	innerPos = [
@@ -114,122 +288,4 @@ function setup() {
 	checkpoints = [
 		[createVector(350, 150), createVector(400, 80)]
 	]
-
-	raceTrack = new RaceTrack(innerPos, outerPos, checkpoints);
-
-	newGeneration();
-
-	// noLoop();
-}
-
-function draw() {
-	// Clear the screen
-	push();
-	noStroke();
-	fill(127);
-	rect(0, 0, width, height);
-	pop();
-
-	raceTrack.draw();
-
-	// Cycle through all of the cars
-	for (let i = 0; i < cars.length; i++) {
-		if (cars[i].alive) {
-			// Read the inputs
-			let rays = cars[i].raycast(raceTrack, false);
-			let action = cars[i].think(rays[1]);
-			cars[i].move(action);
-			cars[i].score++;
-
-			// Kill the car if it is hitting the track
-			if (cars[i].isColliding(raceTrack)) {
-				cars[i].alive = false;
-				deadCars++;
-			} else {
-				// Draw the car and the raycasts
-				cars[i].draw();
-				if (drawRaycasts) {
-					cars[i].raycast(raceTrack, true);
-				}
-			}
-
-		}
-		// else {
-		// 	deadCars++;
-		// }
-	}
-	if (deadCars === cars.length) {
-		newGeneration();
-	}
-	// deadCars = 0;
-}
-
-
-function linesCross(line1, line2) {
-	let x1 = line1[0].x;
-	let y1 = line1[0].y;
-	let x2 = line1[1].x;
-	let y2 = line1[1].y;
-
-	let x3 = line2[0].x;
-	let y3 = line2[0].y;
-	let x4 = line2[1].x;
-	let y4 = line2[1].y;
-
-	// Calculate t and u
-	let den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	let tNum = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
-	let uNum = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3));
-	let t = tNum / den;
-	let u = uNum / den;
-
-	// Return true if the intersection is within the two line segments
-	if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function newGeneration() {
-	// Create the correct amount of cars
-	if (cars.length === 0) {
-		for (let i = 0; i < amount; i++) {
-			cars.push(new Car(createVector(360, 120), 0.52, color(200, 63)));
-		}
-	} else {
-		let bestScore = [cars[0].score, 0];
-		let currentscore;
-		for (let i = 0; i < cars.length; i++) {
-			currentScore = cars[i].score;
-			if (currentScore > bestScore[0]) {
-				bestScore = [currentScore, i];
-			}
-		}
-		let nn = cars[bestScore[1]].nn.serialize();
-		cars[amount - 1] = new Car(createVector(360, 120), 0.52, color(0, 0, 255, 127));
-		cars[amount - 1].nn = NeuralNetwork.deserialize(nn);
-		// console.log(nn.serialize());
-
-		for (let i = 0; i < cars.length - 1; i++) {
-			cars[i] = new Car(createVector(360, 120), 0.52, color(200, 63));
-			cars[i].nn = NeuralNetwork.deserialize(nn);
-			// console.log(cars[i].nn.serialize());
-			cars[i].nn.mutate(mutateItem);
-		}
-		// noLoop();
-	}
-
-	generation++;
-	deadCars = 0;
-}
-
-function mutateItem(x) {
-  if (random(1) < 0.05) {
-    let offset = randomGaussian() * 512;
-    let newx = x + offset;
-    return newx;
-  } else {
-    return x;
-  }
 }
