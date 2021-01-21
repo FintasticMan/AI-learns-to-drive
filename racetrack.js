3d7d6b648e3437f2add48600844a41a6ac10983a
FintasticMan <52415484+FintasticMan@users.noreply.github.com>
2021-01-21 10:04:25 +0100

    Revert "Merge branch 'main' into checkpoints"
    This reverts commit 178352477c3195fac99497c9152d8bddf45358c3, reversing
    changes made to c0d0fd37b19ac942c29558ea88693e1c162fa7f9.

--- racetrack.js
+++ racetrack.js
@@ -1,7 +1,8 @@
 class RaceTrack {
-	constructor(innerPos, outerPos) {
+	constructor(innerPos, outerPos, checkpointsPos) {
 		this.innerWalls = [];
 		this.outerWalls = [];
+		this.checkpoints = [];
 
 		for (let i = 0; i < innerPos.length; i++) {
 			this.innerWalls.push(new Wall(innerPos[i]));
@@ -9,6 +10,10 @@ class RaceTrack {
 
 		for (let i = 0; i < outerPos.length; i++) {
 			this.outerWalls.push(new Wall(outerPos[i]));
+		}
+
+		for (let i = 0; i < checkpointsPos.length; i++) {
+			this.checkpoints.push(new Checkpoint(checkpointsPos[i]));
 		}
 	}
 
@@ -19,6 +24,10 @@ class RaceTrack {
 
 		for (let i = 0; i < this.outerWalls.length; i++) {
 			this.outerWalls[i].draw();
+		}
+
+		for (let i = 0; i < this.checkpoints.length; i++) {
+			this.checkpoints[i].draw();
 		}
 	}
 }
